295e9a004c6dd23fbc40c04fe62de52741f38738
Barnegraver <andreasnilsen07@hotmail.com>
2025-05-25 16:22:36 +0200

    Created helloworld in rust.

---
+++ helloworld/target/debug/deps/helloworld.d
@@ -0,0 +1,5 @@
+C:\Users\ander\Desktop\Rust\helloworld\target\debug\deps\helloworld.exe: src\main.rs
+
+C:\Users\ander\Desktop\Rust\helloworld\target\debug\deps\helloworld.d: src\main.rs
+
+src\main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+C:\Users\ander\Desktop\Rust\helloworld\target\debug\helloworld.exe: C:\Users\ander\Desktop\Rust\helloworld\src\main.rs
